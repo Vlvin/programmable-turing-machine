4cc890b887d1fb4e5b21524e2b1c6cac62917275
Vlvin <ghostv.work@gmail.com>
2026-09-03 14:39:54 +0400

    Turing Machine framed

--- main.cpp
+++ main.cpp
@@ -1,26 +1,52 @@
 #include <deque>
 #include <iostream>
 #include <map>
+#include <tuple>
 #include <vector>
 
 template <class A> class TuringMachine {
 public:
-  typedef enum {
-    LEFT,
-    RIGHT,
-    STAY
-  } Move;
-  typedef std::map<A, std::pair<A, Move>> Q;
+  typedef enum { LEFT, RIGHT, STAY } Move;
+  typedef std::map<A, std::tuple<size_t, A, Move>> Q;
+
+  bool process_cell() {
+    if (states[state].size() == 0)
+      return false;
+    auto &neostate = states[state][*position];
+    *position = std::get<A>(neostate);
+    state = std::get<size_t>(neostate);
+    switch (std::get<Move>(neostate)) {
+    case LEFT:
+      if (position == tape.begin())
+        tape.push_front(A::LAMBDA);
+      position--;
+      break;
+    case RIGHT:
+      if (position + 1 == tape.end())
+        tape.push_back(A::LAMBDA);
+      position++;
+      break;
+    case STAY:
+      break;
+    }
+    return true;
+  }
+
 private:
   std::vector<Q> states;
+  size_t state;
   std::vector<A> values;
   std::deque<A> tape;
   decltype(tape.begin()) position;
 };
 
-int main() { 
-  std::deque<int> deq = {0,1,2,3};
-  auto i = deq.begin();
-  deq.push_front(-1);
-  std::cout << *i << std::endl;
+enum MyA {
+  LAMBDA = '^',
+  ONE = '1',
+  STAR = '*',
+};
+
+int main() {
+  TuringMachine<MyA> tm;
+  tm.process_cell();
 }
