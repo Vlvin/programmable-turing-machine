2a5d1386b3c52696d1ec24a7d053a8424621e102
Vlvin <ghostv.work@gmail.com>
2026-09-04 18:06:46 +0400

    main.cpp

--- main.cpp
+++ main.cpp
@@ -80,26 +80,41 @@ enum MyA {
 typedef ::TuringMachine<MyA>::Move Move;
 
 int main() {
-  TuringMachine<MyA> tm({ONE, ONE, STAR, ONE, ONE},
+#define P 7
+  TuringMachine<MyA> tm({ONE, ONE, ONE, ONE, ONE, ONE, STAR, ONE, ONE, ONE},
                         {
                             // vector
                             { // 0
                                 // of maps
-                                {ONE, {0, ONE, Move::RIGHT}}, // of pairs
-                                {STAR, {1, ONE, Move::LEFT}},
+                                {ONE, {1, LAMBDA, Move::RIGHT}}, // of pairs
+                                {STAR, {4, LAMBDA, Move::RIGHT}},
                             },
-                            { 
-                                {ONE, {1, ONE, Move::LEFT}},
-                                {LAMBDA, {2, LAMBDA, Move::RIGHT}},
+                            { // 1
+                                {ONE, {1, ONE, Move::RIGHT}},
+                                {STAR, {1, STAR, Move::RIGHT}},
+                                {LAMBDA, {2, LAMBDA, Move::LEFT}},
                             },
-                            { 
-                                {ONE, {3, LAMBDA, Move::RIGHT}},
+                            { // 2
+                                {ONE, {3, LAMBDA, Move::LEFT}},
+                                {STAR, {6, ONE, Move::RIGHT}}, // difference 5->6
                             },
-                            { 
+                            { // 3
+                                {ONE, {3, ONE, Move::LEFT}},
+                                {STAR, {3, STAR, Move::LEFT}},
+                                {LAMBDA, {0, LAMBDA, Move::RIGHT}},
+                            },
+                            { // 4
                                 {ONE, {4, LAMBDA, Move::RIGHT}},
+                                {LAMBDA, {P, ONE, Move::STAY}},
+                            },
+                            { // 5
+                                {LAMBDA, {P, LAMBDA, Move::RIGHT}},
+                                {ONE, {5, ONE, Move::LEFT}},
+                            },
+                            { // 6
+                              {LAMBDA, {5, ONE, Move::LEFT}} 
                             },
                             { // empty is P
-
                             },
                         });
   tm.run();
