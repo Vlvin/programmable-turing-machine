c5377bfb2b86f7ab035be547874d121cec46ad56
Vlvin <ghostv.work@gmail.com>
2026-09-10 19:30:06 +0400

    next state is now accepted by index or by function and P can be evaluated dynamically

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <linux/stddef.h>
 #include <turing_machine.cpp>
 
 enum MyA {
@@ -6,49 +7,46 @@ enum MyA {
   STAR = '*',
 };
 
-
-
 typedef ::TuringMachine<MyA>::Move Move;
 
-#define S(on, ch_to, move, next_state)                                         \
+#define Arr(on, ch_to, move, next_state)                                       \
   {                                                                            \
     (on), { (ch_to), (move), (next_state) }                                    \
   }
-
 int main() {
-#define P 5
+#define P [&tm]() { return tm.P(); } // P counts automatically
   TuringMachine<MyA> tm("^111*111^",
                         {
                             {
                                 // 0
-                                S(ONE, LAMBDA, Move::RIGHT, 1), // of pairs
-                                S(STAR, ONE, Move::STAY, P),
+                                Arr(ONE, LAMBDA, Move::RIGHT, 1), // of pairs
+                                Arr(STAR, ONE, Move::STAY, P),
                             },
                             {
                                 // 1
-                                S(ONE, ONE, Move::RIGHT, 1),
-                                S(STAR, STAR, Move::RIGHT, 1),
-                                S(LAMBDA, LAMBDA, Move::LEFT, 2),
+                                Arr(ONE, ONE, Move::RIGHT, 1),
+                                Arr(STAR, STAR, Move::RIGHT, 1),
+                                Arr(LAMBDA, LAMBDA, Move::LEFT, 2),
                             },
                             {
                                 // 2
-                                S(ONE, LAMBDA, Move::LEFT, 3),
-                                S(STAR, ONE, Move::LEFT, 4),
+                                Arr(ONE, LAMBDA, Move::LEFT, 3),
+                                Arr(STAR, ONE, Move::LEFT, 4),
                             },
                             {
                                 // 3
-                                S(ONE, ONE, Move::LEFT, 3),
-                                S(STAR, STAR, Move::LEFT, 3),
-                                S(LAMBDA, LAMBDA, Move::RIGHT, 0),
+                                Arr(ONE, ONE, Move::LEFT, 3),
+                                Arr(STAR, STAR, Move::LEFT, 3),
+                                Arr(LAMBDA, LAMBDA, Move::RIGHT, 0),
                             },
                             {
                                 // 4
-                                S(ONE, ONE, Move::LEFT, 4),
-                                S(LAMBDA, ONE, Move::STAY, P),
+                                Arr(ONE, ONE, Move::LEFT, 4),
+                                Arr(LAMBDA, ONE, Move::STAY, P),
                             },
                             {
-                                // empty is P and it happen to be 7
+                                // empty is P and it happen to be 5
                             },
                         });
-  tm.run();
+ tm.run();
 }
--- src/turing_machine.cpp
+++ src/turing_machine.cpp
@@ -1,34 +1,41 @@
 #include <algorithm>
 #include <deque>
+#include <functional>
 #include <iostream>
 #include <map>
 #include <tuple>
+#include <variant>
 #include <vector>
 
 template <class A> class TuringMachine {
 public:
   typedef enum { LEFT, RIGHT, STAY } Move;
-  typedef std::map<A, std::tuple<A, Move, size_t>> Q;
+  typedef std::function<size_t(void)> state_i_fun_t;
+  typedef std::variant<int, state_i_fun_t> state_i_t;
+  typedef std::map<A, std::tuple<A, Move, state_i_t>> Q;
 
   TuringMachine(const std::deque<A> &init, const std::vector<Q> &q)
       : tape(init), q(q), state(0),
         position(std::find_if(tape.begin(), tape.end(),
                               [](const A &a) { return a != A::LAMBDA; })) {}
   TuringMachine(const std::string &init, const std::vector<Q> &q)
-      : tape(init.size()), q(q), state(0),
-        position(tape.begin()) {
-          for (int i = 0; i < init.size(); i++)
-            tape[i] = (A)init[i];
-        position = std::find_if(tape.begin(), tape.end(),
-                              [](const A &a) { return a != A::LAMBDA; });
-        }
+      : tape(init.size()), q(q), state(0), position(tape.begin()) {
+    for (int i = 0; i < init.size(); i++)
+      tape[i] = (A)init[i];
+    position = std::find_if(tape.begin(), tape.end(),
+                            [](const A &a) { return a != A::LAMBDA; });
+  }
 
   bool process_cell() {
     if (q[state].size() == 0)
       return false;
     auto &neostate = q[state][*position];
     *position = std::get<A>(neostate);
-    state = std::get<size_t>(neostate);
+    auto state_var = std::get<state_i_t>(neostate);
+    if (std::holds_alternative<int>(state_var))
+      state = std::get<int>(state_var);
+    else
+      state = std::get<state_i_fun_t>(state_var)();
     switch (std::get<Move>(neostate)) {
     case LEFT:
       if (position == tape.begin())
@@ -57,11 +64,12 @@ public:
     while (this->process_cell())
       std::cout << *this;
   }
-  friend std::ostream& operator<<(std::ostream& os, const TuringMachine<A> &tm) {
+  friend std::ostream &operator<<(std::ostream &os,
+                                  const TuringMachine<A> &tm) {
     for (auto ii = tm.tape.begin(); ii != tm.tape.end(); ii++) {
       if (ii == tm.position)
         os << '[';
-      else if (ii-1 != tm.position)
+      else if (ii - 1 != tm.position)
         os << ' ';
       os << (char)*ii;
       if (ii == tm.position)
@@ -71,9 +79,15 @@ public:
     return os;
   }
 
+  const size_t P() const {
+    if (q.size() == 0)
+      return -1;
+    return q.size() - 1;
+  }
+
 private:
   std::vector<Q> q;
-  size_t state;
+  int state;
   std::deque<A> tape;
   decltype(tape.begin()) position;
 };
